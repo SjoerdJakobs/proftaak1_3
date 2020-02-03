f03e9b017b3213a829f7ad01740a844b2b348300
Timo van Sark <th.vansark@student.avans.nl>
2020-02-03 13:07:49 +0100

    begin aan agenda

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,10 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 import org.jfree.fx.ResizableCanvas;
@@ -17,16 +19,35 @@ import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
 public class Main extends Application {
-    ResizableCanvas canvas;
-    public Point2D position = new Point2D.Double(100,100);
+    private ResizableCanvas canvas;
+    private VBox vBox;
+    public Point2D position = new Point2D.Double(100, 100);
+
+    private int rows = 8;
+    private int columns = 12;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         BorderPane mainPane = new BorderPane();
         MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().addAll(new Menu("Agenda"),new Menu("Simulation"));
+        menuBar.getMenus().addAll(new Menu("Agenda"), new Menu("Simulation"));
+
+        BorderPane borderPane = new BorderPane();
+
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
-        mainPane.setCenter(canvas);
+        vBox = new VBox();
+        borderPane.setRight(canvas);
+        borderPane.setLeft(vBox);
+
+
+        String[] rowLabels = {"unos","dos","tres", "quadros", "vijf"};
+
+        for (int i = 0; i < rowLabels.length && i < this.rows; i++) {
+            this.vBox.getChildren().add(new Label(rowLabels[i]));
+        }
+
+
+        mainPane.setCenter(borderPane);
         mainPane.setTop(menuBar);
         primaryStage.setScene(new Scene(mainPane));
         primaryStage.setTitle("Block Dragging");
@@ -41,12 +62,21 @@ public class Main extends Application {
 
     public void draw(FXGraphics2D g2d) {
         g2d.setBackground(Color.white);
-        g2d.clearRect(0,0,1920,1080);
+        g2d.clearRect(0, 0, 1920, 1080);
         g2d.setStroke(new BasicStroke(1));
-        g2d.draw(new Rectangle2D.Double(position.getX()-50, position.getY()-50, 100, 100));
-        int stepsize = (int)canvas.getWidth()/20;
-        for(int i =0; i<20; i++){
-            g2d.drawLine((int)stepsize*i,0,(int)stepsize*i,(int)canvas.getHeight());
+        g2d.draw(new Rectangle2D.Double(position.getX() - 50, position.getY() - 50, 100, 100));
+        int vertStepsize = (int) ((canvas.getWidth() - vBox.getWidth()) / this.columns);
+        int horStepsize = (int) canvas.getHeight() / this.rows;
+
+        //Horizontal lines
+        for (int i = 0; i < this.rows; i++) {
+            g2d.drawLine(0, horStepsize * i, (int) canvas.getWidth(), horStepsize * i);
+        }
+
+        //Vertical Lines
+        for (int i = 0; i < this.columns; i++) {
+            g2d.drawLine(vertStepsize * i, 0, vertStepsize * i, (int) canvas.getHeight());
+            g2d.drawString((i + 8) + ":00", i * vertStepsize, 10);
         }
     }
 
@@ -61,7 +91,6 @@ public class Main extends Application {
         position = new Point2D.Double(e.getX(), e.getY());
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
     }
-
 
 
     public static void main(String[] args) {
