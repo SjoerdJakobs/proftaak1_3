65a53f44de9e690fa095aaf8ef1f015b11335e94
Timo van Sark <th.vansark@student.avans.nl>
2020-02-18 13:30:29 +0100

    implemented scrolling

--- src/TiledParser/Simulation.java
+++ src/TiledParser/Simulation.java
@@ -4,6 +4,7 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
@@ -17,6 +18,7 @@ public class Simulation extends Application {
     private Canvas canvas;
     private FXGraphics2D graphics2D;
     private TileMap tileMap;
+    private double scale = 10.0;
 
     public void init() {
         try {
@@ -44,9 +46,10 @@ public class Simulation extends Application {
 
         this.canvas = new Canvas(stage.getWidth(), stage.getHeight());
         this.graphics2D = new FXGraphics2D(canvas.getGraphicsContext2D());
-
         BorderPane mainPane = new BorderPane();
         mainPane.setCenter(canvas);
+
+        canvas.setOnScroll(e -> onMouseScoll(e));
 
         draw(graphics2D);
         stage.setScene(new Scene(mainPane));
@@ -56,13 +59,30 @@ public class Simulation extends Application {
 
     public void draw(FXGraphics2D graphics) {
         graphics.setTransform(new AffineTransform());
-        graphics.setBackground(Color.black);
-        //graphics.scale(10,10);
+        graphics.setBackground(new Color(17, 17, 17));
+        graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        graphics.scale(scale, scale);
 
         tileMap.draw(graphics);
     }
 
     public void update(FXGraphics2D graphics) {
 
+    }
+
+    public void onMouseScoll(ScrollEvent event) {
+        if (event.getDeltaY() < 0) {
+            //Scroll back
+            if (scale > 1) {
+                scale--;
+            }
+        } else if (event.getDeltaY() > 0) {
+            //Scroll forward
+            if (scale < 15) {
+                scale++;
+            }
+        }
+        update(graphics2D);
+        draw(graphics2D);
     }
 }
