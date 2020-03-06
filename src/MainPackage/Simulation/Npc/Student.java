c5f261b038a71f1db299adbad1801c9e229df369
Teun Luijken <teunluijken+gitkraken@gmail.com>
2020-03-06 14:57:06 +0100

    soep

--- src/MainPackage/Simulation/Npc/Student.java
+++ src/MainPackage/Simulation/Npc/Student.java
@@ -16,9 +16,7 @@ public class Student extends Npc{
 
 
     private void initializeSprites(){
-
-
-
+        
     }
 
 
--- src/MainPackage/Simulation/Npc/Teacher.java
+++ src/MainPackage/Simulation/Npc/Teacher.java
@@ -15,7 +15,6 @@ public class Teacher extends Npc {
     }
 
     private void initializeSprites(){
-
         for (int j = 0; j < 4; j++) {
             for (int i = 0; i < 3; i++) {
                 mySprites[i + j * 4] = super.spriteSheet[48 + 12 * j + i];
