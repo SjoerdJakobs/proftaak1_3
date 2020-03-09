b59ff9ca07212a8b8c6083cf535480e17843659c
Teun Luijken <teunluijken+gitkraken@gmail.com>
2020-03-09 11:06:19 +0100

    mysprites rebased to NPC

--- src/MainPackage/Simulation/Npc/Npc.java
+++ src/MainPackage/Simulation/Npc/Npc.java
@@ -4,6 +4,7 @@ import MainPackage.Simulation.Logic.LogicalTile;
 
 import OOFramework.FrameworkProgram;
 import OOFramework.StandardObject;
+import org.jfree.fx.FXGraphics2D;
 
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
@@ -12,12 +13,14 @@ public class Npc extends StandardObject {
 protected LogicalTile currentTile;
 protected Point2D cords;
 protected BufferedImage[] spriteSheet = SPRITESHEET.Sprites;
+protected  BufferedImage[] mySprites = new BufferedImage[12];
+protected FXGraphics2D graphics2D;
 
 
-    protected Npc(FrameworkProgram frameworkProgram) {
+    protected Npc(FrameworkProgram frameworkProgram, FXGraphics2D graphics2D) {
         super(frameworkProgram);
         initializeSprites();
-
+        this.graphics2D = graphics2D;
     }
 
 
@@ -32,12 +35,19 @@ protected BufferedImage[] spriteSheet = SPRITESHEET.Sprites;
     }
 
     @Override
-    protected void RenderLoop(double deltaTime){}
+    protected void RenderLoop(double deltaTime){
+
+
+
+    }
 
 
     protected void initializeSprites(){
-
     }
+
+
+    
+
 
 
 
--- src/MainPackage/Simulation/Npc/Student.java
+++ src/MainPackage/Simulation/Npc/Student.java
@@ -1,16 +1,17 @@
 package MainPackage.Simulation.Npc;
 
 import OOFramework.FrameworkProgram;
+import org.jfree.fx.FXGraphics2D;
 
 import java.awt.image.BufferedImage;
 import java.util.Random;
 
 
 public class Student extends Npc {
-    private BufferedImage[] mySprites = new BufferedImage[12];
+    //private BufferedImage[] mySprites = new BufferedImage[12];
 
-    protected Student(FrameworkProgram frameworkProgram) {
-        super(frameworkProgram);
+    protected Student(FrameworkProgram frameworkProgram, FXGraphics2D graphics2D) {
+        super(frameworkProgram, graphics2D);
     }
 
     @Override
@@ -43,7 +44,7 @@ public class Student extends Npc {
 
         for (int j = 0; j < 4; j++) {
             for (int i = 0; i < 3; i++) {
-                mySprites[i + j * 4] = super.spriteSheet[12 * j + i + spriteOffset];
+                super.mySprites[i + j * 4] = super.spriteSheet[12 * j + i + spriteOffset];
             }
         }
 
